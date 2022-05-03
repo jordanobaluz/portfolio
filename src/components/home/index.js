b04d1b9b0eb76513f85314e5c787c9ad55943f91
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-03 15:24:17 -0300

    imported the components into the home, defined the css rules for the home component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./home.css";
 import Header from "../header/index";
+import Body from "../body";
+import Footer from "../footer";
 
 function Home() {
   return (
     <div className="home">
       <div>
         <Header />
+      </div>
+      <div>
+        <Body />
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
