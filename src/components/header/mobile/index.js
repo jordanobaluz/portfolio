f29e1be20e56feb0cbd509a657b3b9192ab0f318
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-03 16:45:35 -0300

    created the mobile menu component and the css rules

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./mobile.css";
 
-function Mobile() {
-  return <div>mobile</div>;
+function Mobile({ isOpen, setIsOpen }) {
+  return (
+    <div className="mobile">
+      <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
+        <i class="fi fi-rr-cross"></i>
+      </div>
+      <div className="mobile-options">
+        <div className="mobile-option">
+          <a href="#project">
+            <i class="fi fi-rr-edit-alt option-icon"></i>
+            Projetos
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#skills">
+            <i class="fi fi-rr-computer option-icon"></i>
+            Habilidades
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#work">
+            <i class="fi fi-rr-briefcase option-icon"></i>
+            Trabalho
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#contact">
+            <i class="fi fi-rr-portrait option-icon"></i>
+            Contato
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mobile;
