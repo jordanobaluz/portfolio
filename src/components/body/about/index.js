4a4b5c69ec3522556c0570197f003ca68b7e6264
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-04 11:43:00 -0300

    created the new component for social contact, defined the structure for the about component and defined the css rules

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./about.css";
+import SocialContact from "../../common/social-contact";
 
 function About() {
-  return <div className="about">hi from the about page</div>;
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          <h4>Olá, sou Jordano Baluz</h4>
+          Sou programador web, atualmente estudando frontend com ReactJS
+        </div>
+        <div className="about-photo">
+          <img
+            src={"images/web-coding.png"}
+            alt="web-coding"
+            className="picture"
+          />
+        </div>
+      </div>
+      <SocialContact />
+    </div>
+  );
 }
 
 export default About;
---
+++ src/components/common/social-contact/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./social-contact.css";
+
+function SocialContact() {
+  return <div className="social-contact">Hi from the social contact page</div>;
+}
+
+export default SocialContact;
