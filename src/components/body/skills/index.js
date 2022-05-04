65c8364ed34cfeb6cccb8af3562102fc93e5ad83
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-04 15:30:33 -0300

    inicialized the skills component, pointed the separator section in the skills component and created the skills data

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./skills.css";
+import SeparatorSections from "../../common/separatorSections";
 
 function Skills() {
-  return <div className="skills">hi from the skills page</div>;
+  return (
+    <div className="skills">
+      <SeparatorSections />
+      <label className="section-tittle">Skills</label>
+      <div></div>
+    </div>
+  );
 }
 
 export default Skills;
---
+++ src/components/data/skills.js
@@ -0,0 +1,32 @@
+export const SkillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "HTML",
+        icon: <i class="devicon-html5-plain colored"></i>,
+      },
+      {
+        name: "CSS",
+        icon: <i class="devicon-css3-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    list: [
+      {
+        name: "SQL",
+        icon: <i class="devicon-mysql-plain-wordmark colored"></i>,
+      },
+    ],
+  },
+];
