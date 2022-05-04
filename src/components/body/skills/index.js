20a7de88a5ccc7774355fd1a73cdc61f99dbc19c
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-04 15:49:27 -0300

    finished the skill card component and the skills component, defined the css rules for both

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./skills.css";
 import SeparatorSections from "../../common/separatorSections";
+import { SkillsData } from "../../data/skills";
+import SkillCard from "./skill-card";
 
 function Skills() {
+  const data = SkillsData;
   return (
     <div className="skills">
       <SeparatorSections />
       <label className="section-tittle">Skills</label>
-      <div></div>
+      <div className="skills-container">
+        {data.map((item) => {
+          return (
+            <div className="skills-section">
+              <label className="skills-section-tittle">{item.type}</label>
+              <div className="skills-list">
+                {item.list.map((skill) => {
+                  return <SkillCard skill={skill} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
