6717f03bf6f5a1ed34b6616503149e58baa29d31
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-04 17:20:34 -0300

    finalized the footer component and defined css rules

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./footer.css";
 
 function Footer() {
-  return <div>hi from the footer</div>;
+  return <div className="footer">Desenvolvido por Jordano Baluz - 2022</div>;
 }
 
 export default Footer;
