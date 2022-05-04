41e28339b9a0e961d42e184ad506300cae3fcdb0
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-04 15:14:25 -0300

    created the commom separator section component with gradient color generate from colorspace

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SeparatorSections from "../../common/separatorSections";
 import { ProjectData } from "../../data/projects";
 import ProjectCard from "./project-card";
 import "./projects.css";
@@ -7,6 +8,7 @@ function Projects() {
   const data = ProjectData;
   return (
     <div className="projects">
+      <SeparatorSections />
       <label className="section-title">Projetos</label>
       <div>
         {data.map((project) => {
---
+++ src/components/common/separatorSections/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./separator.css";
+
+function SeparatorSections() {
+  return <div className="separator"></div>;
+}
+
+export default SeparatorSections;
