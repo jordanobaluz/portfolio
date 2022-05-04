33b5d489979e250e93f32b8a2762af240ef3b3e0
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-04 16:39:56 -0300

    inicialized the work component, imported the separator component into work and created the work data file that will be imported in work component

--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./work.css";
+import SeparatorSection from "../../common/separatorSections";
 
 function Work() {
-  return <div className="work">hi from the work page</div>;
+  return (
+    <div className="work">
+      <SeparatorSection />
+      <label className="section-tittle">Trabalhos</label>
+    </div>
+  );
 }
 
 export default Work;
---
+++ src/components/data/data.js
@@ -0,0 +1,20 @@
+export const WorkData = [
+  {
+    company: "Company Test 1",
+    designation: "Software Engineer",
+    dateStart: "04-05-22",
+    dataEnd: "Present",
+    companyLogo:
+      "https://www.adobe.com/br/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium",
+    work: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab laboriosam mollitia nesciunt omnis. Vitae, alias explicabo! Corporis, neque distinctio tenetur tempora, beatae, maiores ipsum exercitationem adipisci quasi ipsa nam aliquam.",
+  },
+  {
+    company: "Company Test 2",
+    designation: "Software Engineer",
+    dateStart: "04-05-21",
+    dataEnd: "01-05-22",
+    companyLogo:
+      "https://www.adobe.com/br/express/create/media_13f3783f8237cbb3ea1a7cf2e7d6f18ad17f36fed.jpeg?width=400&format=webply&optimize=medium",
+    work: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab laboriosam mollitia nesciunt omnis. Vitae, alias explicabo! Corporis, neque distinctio tenetur tempora, beatae, maiores ipsum exercitationem adipisci quasi ipsa nam aliquam.",
+  },
+];
