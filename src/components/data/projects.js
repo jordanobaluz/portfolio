507ca5bcf706d25448dbfd7c5c8d38390d10c4b1
JordanoBaluz <jordanolb95@hotmail.com>
2022-05-09 16:29:04 -0300

    removed favicon, altered the projects data for real infos

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,24 +1,24 @@
 export const ProjectData = [
   {
     id: 1,
-    tittle: "Teste project data 1",
+    tittle: "Projeto do livro, The road to learn React",
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eaque enim veniam quia, nostrum consequatur sint tempore fuga rem vel quisquam! Quos earum quis fugit explicabo sed, minus non placeat.",
-    tags: ["ReactJS", "NodeJS"],
-    demo: "demotest",
-    github: "githubtest",
+      "O projeto contempla um SPA (Single Page Application) que faz comunicação com a API Hacker News, permitindo ao usuário pesquisar por palavras chaves e ter acesso a diversas notícias relacionadas a pesquisa. Existem filtros que permitem filtrar por Título (Title), Autor (Author), Comentários (Comments) e Pontos (Points)",
+    tags: ["ReactJS", "JavaScript", "CSS", "HTML"],
+    demo: "https://whispering-dawn-22201.herokuapp.com/",
+    github: "https://github.com/JordanoBaluz/roadToReact",
     image:
-      "https://i.pinimg.com/564x/f1/8b/d7/f18bd70dcddbad6d2d13675cdc73a568.jpg",
+      "https://camo.githubusercontent.com/31e0905eabbbf53658d7224b5fa94988dc375682df2b5df3d346b85437681bd3/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f6f4d7865575a4131396d746330447346346c2f67697068792e676966",
   },
   {
     id: 2,
-    tittle: "Teste project data 2",
+    tittle: "Jogo de Resgate",
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eaque enim veniam quia, nostrum consequatur sint tempore fuga rem vel quisquam! Quos earum quis fugit explicabo sed, minus non placeat.",
-    tags: ["HTML", "CSS", "JS", "ReactJS", "NodeJS"],
+      "O projeto consiste de um game 2D onde se controla um helicóptero que tem como missão resgatar seu amigo, enquanto precisa derrotar os inimigos.",
+    tags: ["JavaScript", "HTML", "CSS", "JQuery"],
     demo: "demotest",
-    github: "githubtest",
+    github: "https://github.com/JordanoBaluz/shipGame",
     image:
-      "https://i.pinimg.com/564x/16/56/91/16569161e74e2519751ba36fb690f12a.jpg",
+      "https://camo.githubusercontent.com/3cc33f97dfef5a87e185261d9d416356dbee887b622f26930cd04f853d437eff/68747470733a2f2f6d65646961322e67697068792e636f6d2f6d656469612f4a6b476b504f69524671436d3633705251562f67697068792e6769663f6369643d373930623736313161663235666339396239326465616130626464326232356230643833666266623838373530313864267269643d67697068792e6769662663743d67",
   },
 ];
